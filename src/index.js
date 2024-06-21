9ef284a1fb75be1211876635ffad2a412966c985
Russell Moore <russellkmoore@users.noreply.github.com>
2024-06-20 20:41:12 -0700

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.21.8
      project name = akeneo-cloudflare-connector
      package manager = npm@10.2.4
      wrangler = wrangler@3.61.0
      git = 2.39.3 (Apple Git-146)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    return new Response("Hello World!");
+  }
+};
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
